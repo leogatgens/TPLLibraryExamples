70e92741a2fb5b33b5958368f12ff242898de61d
Leonardo Meléndez <leogatgens@gmail.com>
2018-07-04 12:17:19 -0600

    Initial project

---
+++ TaskTPL-Ccharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTPL_Ccharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
